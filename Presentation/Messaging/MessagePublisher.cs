2efd1b46a25c854bebced9d44b94c4c398fb43cf
amarjeet-kaloty <amarjeet.kaloty@yahoo.com>
2025-10-07 07:30:26 -0400

    Naming conflicts clarification.

--- Presentation/Messaging/MessagePublisher.cs
+++ Presentation/Messaging/MessagePublisher.cs
@@ -11,7 +11,9 @@ namespace Presentation.Messaging
         private readonly ILogger<MessagePublisher> _logger;
         private readonly IConnectionFactory _connectionFactory;
         private const string PUB_SUB_COMPONENT = "rabbitmq-pubsub";
+        private const string EXCHANGE_NAME = "employee-data-changes";
         private const string TOPIC_NAME = "employee_events";
+        private const string UPDATED_TOPIC_NAME = "employee-updated-topic";
 
         public MessagePublisher(ILogger<MessagePublisher> logger, DaprClient daprClient, IConnectionFactory connectionFactory)
         {
@@ -32,14 +34,14 @@ namespace Presentation.Messaging
             var connection = await _connectionFactory.CreateConnectionAsync();
             var channel = await connection.CreateChannelAsync();
 
-            await channel.ExchangeDeclareAsync(exchange: "rabbitmq-pubsub", type: ExchangeType.Topic);
+            await channel.ExchangeDeclareAsync(exchange: EXCHANGE_NAME, type: ExchangeType.Topic);
 
             var message = JsonSerializer.Serialize(employeeData);
             var body = Encoding.UTF8.GetBytes(message);
 
             await channel.BasicPublishAsync(
-                exchange: "rabbitmq-pubsub",
-                routingKey: "employee-updated-topic",
+                exchange: EXCHANGE_NAME,
+                routingKey: UPDATED_TOPIC_NAME,
                 body: body);
             _logger.LogInformation($" [=>] Sent employee update event to RabbitMQ component... {message}");
         }
