8fd7b5e713c82c356c256c51d9852bedb5914714
amarjeet-kaloty <amarjeet.kaloty@yahoo.com>
2025-10-31 10:30:45 -0400

    customized the schema to reflect different data types.

--- Application/DTOs/CreateEmployeeDTO.cs
+++ Application/DTOs/CreateEmployeeDTO.cs
@@ -1,13 +1,20 @@
 ﻿
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace EmployeeManagementSolu.Application.DTOs
 {
     public class CreateEmployeeDTO : IRequest<ReadEmployeeDTO>
     {
-        public required string Name { get; set; }
+        [Required]
+        public string Name { get; set; }
         public required string Address { get; set; }
         public required string Email { get; set; }
         public string? Phone { get; set; }
+        public long? Age { get; set; }
+        public decimal? Salary { get; set; }
+        public bool? IsActive { get; set; }
+        public required DateTime JoiningDate { get; set; }
+
     }
 }
